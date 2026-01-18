a579b2af52d1f2a8c47a71a547d637ae34ffc956
mntone <901816+mntone@users.noreply.github.com>
2026-01-18 17:48:33 +0900

    refactor(pages): rename routes module to singular
    - rename routes.ts to route.ts
    - update App import to new module name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { NavigationStackProvider } from '~/navigation/Provider'
 import { NavigationStackRender } from '~/navigation/Render'
 
 import { historyDriver } from './navigation/drivers/history'
-import { initialRouteId, navigationRoutes } from './pages/routes'
+import { initialRouteId, navigationRoutes } from './pages/route'
 
 export function App() {
 	return (
