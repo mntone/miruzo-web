464880862ac8e80c216d288c7d0a90dfbf9a901f
mntone <901816+mntone@users.noreply.github.com>
2026-01-23 16:46:39 +0900

    feat(progress): switch LoadingView to JS delay
    - remove CSS visibility animation for LoadingView
    - show LoadingView after timeout via JS and unmount before delay
    - avoid potential screen reader misses from visibility toggling

--- src/components/progress/LoadingView.css.ts
+++ src/components/progress/LoadingView.css.ts
@@ -1,14 +1,6 @@
-import { keyframes, style } from '@vanilla-extract/css'
-
-const appear = keyframes({
-	to: {
-		visibility: 'visible',
-	},
-})
+import { style } from '@vanilla-extract/css'
 
 export const root = style({
-	animation: `forwards ${appear}`,
 	color: 'var(--text-secondary)',
 	gap: '4px',
-	visibility: 'hidden',
 })
--- src/components/progress/LoadingView.tsx
+++ src/components/progress/LoadingView.tsx
@@ -1,13 +1,9 @@
-import { untrack } from 'solid-js'
+import { createSignal, onCleanup, Show } from 'solid-js'
 
 import { useI18n } from '~/i18n/Context'
 
 import { LoadingIndicator } from './LoadingIndicator'
 import * as styles from './LoadingView.css'
-
-function getDelayInMilliseconds(step: number | undefined): number {
-	return Math.max(0, 221 * (step ?? 3))
-}
 
 interface LoadingViewProps {
 	readonly delayStep?: number
@@ -15,22 +11,33 @@ interface LoadingViewProps {
 }
 
 export function LoadingView(props: LoadingViewProps) {
-	const delay = untrack(function() {
-		return getDelayInMilliseconds(props?.delayStep)
-	})
+	// eslint-disable-next-line solid/reactivity -- fixed at setup
+	const delay = Math.max(0, 221 * (props.delayStep ?? 3))
+	const noDelay = delay === 0
+	const [getIsVisible, setIsVisible] = createSignal(noDelay)
+
+	if (!noDelay) {
+		const timeoutId = window.setTimeout(function() {
+			setIsVisible(true)
+		}, delay)
+		onCleanup(function() {
+			window.clearTimeout(timeoutId)
+		})
+	}
 
 	const { t } = useI18n()
 	return (
-		<div
-			class={/* @once */ 'vstack ' + styles.root}
-			role='status'
-			style={{
-				'animation-delay': `${delay}ms`,
-				'height': props.height,
-			}}
-		>
-			<LoadingIndicator size={48} />
-			<span>{t('labels.state_load')}</span>
-		</div>
+		<Show when={getIsVisible()}>
+			<div
+				class={/* @once */ 'vstack ' + styles.root}
+				role='status'
+				style={/* @once */{
+					height: props.height,
+				}}
+			>
+				<LoadingIndicator size={48} />
+				<span>{t('labels.state_load')}</span>
+			</div>
+		</Show>
 	)
 }
